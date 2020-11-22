8fffa5165fc0c683272f83b554eaba917726c924
Gitansh Raj Satija <55681531+gitttsattt@users.noreply.github.com>
2020-11-22 15:44:32 +0530

    Update Ball.java

--- src/Tokens/Ball.java
+++ src/Tokens/Ball.java
@@ -1,103 +1,29 @@
 package Tokens;
 
-import javafx.geometry.Point2D;
-import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Region;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import sample.COLOR;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class Ball extends Region {
-    public Point2D location;
-    public Point2D velocity;
-    Point2D acceleration;
-    double mass;
-    double maxSpeed = 10;
-    Node view;
-    double width;
-    double height;
-    double centerX;
-    double centerY;
-    double radius;
-    Pane layer;
+import javafx.geometry.Point2D;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
-    public Ball(Pane layer, Point2D location, Point2D velocity, Point2D acceleration, double mass, Group ro, COLOR cl) {
-        this.layer = layer;
+public class ColorSwapper {
+    Point2D location;
+    public Node view;
+    public ColorSwapper(Point2D location) throws FileNotFoundException {
         this.location = location;
-        this.velocity = velocity;
-        this.acceleration = acceleration;
-        this.mass = mass;
-        width = mass;
-        height = width;
-        centerX = width / 2.0;
-        centerY = height / 2.0;
-        radius = width / 2.0;
-        Circle circle = new Circle(radius);
-        circle.setCenterX(radius);
-        circle.setCenterY(radius);
-        switch (cl) {
-            case AQUA:
-                circle.setFill(Color.rgb(141, 18, 255));
-                break;
-            case PURPLE:
-                circle.setFill(Color.rgb(53, 226, 242));
-                break;
-            case YELLOW:
-                circle.setFill(Color.rgb(245, 223, 15));
-                break;
-            case PINK:
-                circle.setFill(Color.rgb(255, 0, 132));
-                break;
-        }
-        ro.getChildren().add(circle);
-        ro.getChildren().add(this);
-        this.view = circle;
-        getChildren().add(view);
-
-        layer.getChildren().add(this);
+        this.view = makeImage("images/swap.png", location.getX(), location.getY(), 40, 40, true);
     }
 
-    public void applyForce(Point2D force) {
-        Point2D f = force.multiply(0.07);
-        acceleration = acceleration.add(f);
-    }
-
-    public void move() {
-        velocity = velocity.add(acceleration);
-        if (velocity.magnitude() > maxSpeed) {
-            velocity = velocity.normalize().multiply(maxSpeed);
-        }
-        location = location.add(velocity);
-        acceleration = new Point2D(0, 0);
-    }
-
-    public void bounce() {
-        double locationX = location.getX();
-        double locationY = location.getY();
-        double velocityX = velocity.getX();
-        double velocityY = velocity.getY();
-        System.out.println(velocityY);
-        velocityY = -10;
-        location = new Point2D(locationX, locationY);
-        velocity = new Point2D(velocityX, velocityY);
-    }
-
-    public void checkBounds() {
-        double locationX = location.getX();
-        double locationY = location.getY();
-        double velocityX = velocity.getX();
-        double velocityY = velocity.getY();
-        if (locationY > 675) {
-            locationY = 675;
-            velocityY = 0;
-        }
-        location = new Point2D(locationX, locationY);
-        velocity = new Point2D(velocityX, velocityY);
-    }
-
-    public void display() {
-        relocate(location.getX() - centerX, location.getY() - centerY);
+    ImageView makeImage(String url, double xPos, double yPos, double height, double width, boolean preserveRatio) throws FileNotFoundException {
+        Image img = new Image(new FileInputStream(url));
+        ImageView viewImg = new ImageView(img);
+        viewImg.setX(xPos);
+        viewImg.setY(yPos);
+        viewImg.setFitHeight(height);
+        viewImg.setFitWidth(width);
+        viewImg.setPreserveRatio(preserveRatio);
+        return viewImg;
     }
 }
