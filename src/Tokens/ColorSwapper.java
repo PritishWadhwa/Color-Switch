3fa253a925203a0b307da159ca7cf92797f136dd
Gitansh Raj Satija <55681531+gitttsattt@users.noreply.github.com>
2020-11-22 16:20:51 +0530

    Update ColorSwapper.java

--- src/Tokens/ColorSwapper.java
+++ src/Tokens/ColorSwapper.java
@@ -1,19 +1,19 @@
 package Tokens;
 
+import javafx.geometry.Point2D;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import java.awt.geom.Point2D;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class ColorSwapper {
     Point2D location;
-    Node view;
+    public Node view;
     public ColorSwapper(Point2D location) throws FileNotFoundException {
         this.location = location;
-        this.view = makeImage("images/swap.png", location.getX(), location.getY(), 100, 100, true);
+        this.view = makeImage("images/swap.png", location.getX(), location.getY(), 40, 40, true);
     }
 
     ImageView makeImage(String url, double xPos, double yPos, double height, double width, boolean preserveRatio) throws FileNotFoundException {
